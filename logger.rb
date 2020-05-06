11f1a9fdee4536fb9c64ac49ab96c07e1346acc3
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-05-06 20:29:35 +0800

    Add files via upload

---
+++ logger.rb
@@ -0,0 +1,14 @@
+require 'singleton'
+
+class Logger
+  include Singleton
+
+    def initialize
+      @f = File.open 'log.txt', 'a'
+    end
+
+  def log_something wat
+    @f.puts wat
+    @f.flush
+  end
+end
