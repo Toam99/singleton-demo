cbb416c77dff38b80655ef7dad445c693e92bd84
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-08-18 22:39:13 +0800

    Update logger.rb

--- logger.rb
+++ logger.rb
@@ -2,13 +2,12 @@ require 'singleton'
 
 class Logger
   include Singleton
-
+  
     def initialize
       @f = File.open 'log.txt', 'a'
     end
-
+     
   def log_something wat
     @f.puts wat
-    @f.flush
-  end
+  end  
 end
